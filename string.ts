6e794f570e089ca8d13e67efdb895e3edb3954c2
micheaol <micheaol80@gmail.com>
2021-10-02 14:16:55 +0100

    String and string in .ts file

---
+++ string.ts
@@ -0,0 +1,29 @@
+// let myMessage = "Name";
+
+// console.log(typeof(myMessage));
+
+// //Diefference between String and string
+
+// let songs:string = "I love jesus";
+// let checker = new String("I don't know it");
+
+
+
+// checker = songs;
+
+// console.log(songs);
+// console.log(typeof(songs));
+
+// console.log(checker);
+// console.log(typeof(checker));
+
+//string method:
+// let songs:string = "I love jesus";
+// console.log(songs.concat(" Hey"));
+// console.log(songs.endsWith(" Hey"));
+
+let echoOne = new String("I am here");
+let echoTwo: string = ("I am Two!");
+
+console.log(echoOne.endsWith("?"));
+console.log(echoTwo.endsWith("!"));
