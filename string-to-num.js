2608b3e40fc03e55e17014e6397521bd0f22fe02
micheaol <micheaol80@gmail.com>
2021-10-02 18:12:49 +0100

    strings

---
+++ string-to-num.js
@@ -0,0 +1,5 @@
+// console.log(typeof(+"100"));
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(Number.MIN_SAFE_INTEGER);
+let bigNumber = 2;
+console.log(typeof (bigNumber));
---
+++ string-to-num.ts
@@ -0,0 +1,8 @@
+// console.log(typeof(+"100"));
+
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(Number.MIN_SAFE_INTEGER);
+
+let bigNumber: number | string | boolean = true;
+
+console.log(typeof(bigNumber));
